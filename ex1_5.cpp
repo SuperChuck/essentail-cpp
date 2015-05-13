8b93ab899e44892f6f3e1f8d4a836699c143a837
Chuck <chen.changge@163.com>
2015-05-13 21:31:29 +0800

    cstyle string

--- ex1_5.cpp
+++ ex1_5.cpp
@@ -1,12 +1,15 @@
 #include<iostream>
-#include<string>
+//#include<string>
+#include<cstring>
+#include<iomanip>
 using namespace std;
 
 int main()
 {
+	/*
 	string name;
-	cout<<"Pls input your name: ";
-	cin>>name;
+	cout << "Pls input your name: ";
+	cin >> name;
 	switch(name.size())
 	{
 		case 0:
@@ -20,5 +23,24 @@ int main()
 				cout<<"hello, "<<name 
 					<<"yo whs up"<<endl; 		
 	}
+	*/
+	
+	const int nm_size = 128;
+	char user_name[nm_size];
+	cout << "Pls enter your name: ";
+	cin >> setw(nm_size) >>  user_name;
+	
+	switch(strlen(user_name))
+	{
+		case 127:
+			cout << "that is a very long name indeed----"
+				 << "we may need a shorter one.\n"
+				 << "In any case\n";
+			break;
+	    default:
+			cout << "hello, " << user_name
+				 << " What's up!\n"; 		
+	}
+	
 	return 0;	
 }
